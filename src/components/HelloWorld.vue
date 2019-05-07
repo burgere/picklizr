7dbc958e57ca2e043c2dc8c1bb32876cc7e9230c
Eric Burger <ericburger@Erics-MacBook-Pro.local>
2019-05-07 07:38:57 -0500

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="picklizr__title" v-html="'PICKLIZR'"></div>
+    <div class="picklizr__grid">
+      <div class="grid__box arrow-top-left"></div>
+      <div class="grid__box top" v-html="people[4].name"></div>
+      <div class="grid__box arrow-top-right"></div>
+      <div class="grid__box left" v-html="people[1].name"></div>
+      <div class="grid__box center" v-html="people[0].name"></div>
+      <div class="grid__box right" v-html="people[3].name"></div>
+      <div class="grid__box arrow-bottom-left"></div>
+      <div class="grid__box bottom" v-html="people[2].name"></div>
+      <div class="grid__box arrow-bottom-right"></div>
+    </div>
+    <div class="button__container">
+      <div class="button" v-html="'Picklize'" v-on:click="picklize"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  /* eslint-disable indent */
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        people: [
+          {
+            id: 0,
+            name: 'Nick',
+            isEditing: 1
+          },
+          {
+            id: 1,
+            name: 'Linda',
+            isEditing: 2
+          },
+          {
+            id: 2,
+            name: 'Josh',
+            isEditing: 3
+          },
+          {
+            id: 3,
+            name: 'Eric',
+            isEditing: 4
+          },
+          {
+            id: 4,
+            name: 'Id',
+            isEditing: 1
+          }
+        ]
+      }
+    },
+    methods: {
+      picklize () {
+        console.log('Ni')
+        this.people = this.people.map(person => ({
+          id: person.id,
+          name: person.name,
+          isEditing: this.getRandomInt(1, 5)
+        }))
+          .slice(1, 5)
+          .sort((a, b) => a.isEditing === b.id)
+      },
+      getRandomInt (min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min)) + min
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .picklizr {
+    &__title {
+      text-align: center;
+      font-size: 20px;
+    }
+    &__grid {
+      display: grid;
+      grid-template: "arrow-top-left top arrow-top-right" "left center right" "arrow-bottom-left bottom arrow-bottom-right";
+
+      .grid {
+        &__box {
+          border: solid 1px;
+          height: 150px;
+        }
+      }
+
+      .center {
+        background-image: url('../assets/pickle.png');
+        background-size: cover;
+      }
+    }
+  }
+
+  .button {
+    &__container {
+      display: flex;
+      justify-content: center;
+      width: 100%;
+    }
+
+    height: 50px;
+    padding: 5px 10px;
+    background-color: lightblue;
+    border-radius: 6px;
+  }
+
 </style>
